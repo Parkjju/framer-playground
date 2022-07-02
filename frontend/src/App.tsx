f8e27a5e35cf179e521d6d38d93934a9ccefa6ca
Parkjju <rudwns3927@gmail.com>
2022-07-02 23:12:22 +0900

    ✨ feat: hover animtaion

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ const Wrapper = styled(motion.div)`
     height: 100vh;
     width: 100vw;
     display: flex;
-    background-color: #b00df9;
+    background-color: #8e0bfe;
     justify-content: center;
     align-items: center;
 `;
@@ -13,56 +13,24 @@ const Wrapper = styled(motion.div)`
 const Box = styled(motion.div)`
     width: 200px;
     height: 200px;
-    background-color: #d14cf9;
+    background-color: white;
     border-radius: 40px;
     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    grid-template-rows: repeat(2, 1fr);
-`;
-
-const Circle = styled(motion.div)`
-    width: 70px;
-    height: 70px;
-    background-color: white;
-    border-radius: 35px;
-    place-self: center;
 `;
 
 const boxVariants: Variants = {
-    start: { opacity: 0, scale: 0.5 },
-    end: {
-        opacity: 1,
-        scale: 1,
-        transition: {
-            type: 'spring',
-            duration: 1,
-            bounce: 0.5,
-            staggerChildren: 0.3,
-        },
-    },
-};
-
-const circleVariants = {
-    start: {
-        opacity: 0,
-        y: 20,
-    },
-    end: {
-        y: 0,
-        opacity: 1,
-    },
+    hover: { scale: 2, rotateZ: 90 },
+    click: { borderRadius: '100px', scale: 1 },
 };
 
 function App() {
     return (
         <Wrapper>
-            <Box variants={boxVariants} initial='start' animate='end'>
-                <Circle variants={circleVariants} />
-                <Circle variants={circleVariants} />
-                <Circle variants={circleVariants} />
-                <Circle variants={circleVariants} />
-            </Box>
+            <Box
+                variants={boxVariants}
+                whileHover='hover'
+                whileTap='click'
+            ></Box>
         </Wrapper>
     );
 }
