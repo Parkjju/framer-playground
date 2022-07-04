a073c9892ff34f89b29e80e92800454f0b7dbb23
Parkjju <rudwns3927@gmail.com>
2022-07-04 19:11:04 +0900

    ✨ feat: drag animation, useMotionValue, useTransform

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,25 @@
 import styled from 'styled-components';
-import { motion, Variants } from 'framer-motion';
+import {
+    motion,
+    useViewportScroll,
+    useMotionValue,
+    useTransform,
+    Variants,
+} from 'framer-motion';
+import { useEffect } from 'react';
 
 const Wrapper = styled(motion.div)`
     height: 100vh;
     width: 100vw;
     display: flex;
-    background-color: #8e0bfe;
+    background-color: linear-gradient(
+        135deg,
+        rgb(238, 0, 135),
+        rgb(238, 0, 238)
+    );
     justify-content: center;
     align-items: center;
+    height: 150vh;
 `;
 
 const Box = styled(motion.div)`
@@ -19,20 +31,35 @@ const Box = styled(motion.div)`
 `;
 
 const boxVariants: Variants = {
-    hover: { scale: 2, rotateZ: 90 },
-    click: { borderRadius: '100px', scale: 1 },
+    hover: { rotateZ: 90 },
 };
 
 function App() {
+    const x = useMotionValue(0);
+    const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+    const gradient = useTransform(
+        x,
+        [-800, 0, 800],
+        [
+            'linear-gradient(135deg,rgb(0, 167, 238),rgb(0, 107, 238))',
+            'linear-gradient(135deg,rgb(238, 0, 135),rgb(238, 0, 238))',
+            'linear-gradient(135deg,rgb(0, 238, 147),rgb(0, 238, 44))',
+        ]
+    );
+
+    const { scrollYProgress } = useViewportScroll();
+    const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
+
     return (
-        <Wrapper>
+        <Wrapper style={{ background: gradient }}>
             <Box
-                drag
+                style={{ x, rotateZ, scale }}
+                drag='x'
+                dragSnapToOrigin
                 variants={boxVariants}
                 whileHover='hover'
                 whileTap='click'
-                whileDrag={{ backgroundColor: 'rgb(46,204,113)' }}
-            ></Box>
+            />
         </Wrapper>
     );
 }
