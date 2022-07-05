a54472006b252a9708bf6f3ec79c2ddb20ba2f50
Parkjju <rudwns3927@gmail.com>
2022-07-06 00:32:00 +0900

    ✨ feat: overlay 구현 - layoutId 프롭스 활용

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,73 +12,91 @@ const Wrapper = styled(motion.div)`
         rgb(238, 0, 238)
     );
     align-items: center;
-    justify-content: center;
-    flex-direction: column;
+    justify-content: space-around;
+`;
+
+const Grid = styled.div`
+    display: grid;
+    width: 50vw;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(2, 300px);
+    div:first-child,
+    div:last-child {
+        grid-column: span 2;
+    }
+    gap: 10px;
 `;
 const Box = styled(motion.div)`
-    height: 200px;
-    width: 200px;
-    position: absolute;
-    top: 100px;
-    border-radius: 50px;
+    height: 300px;
+
+    border-radius: 40px;
     background-color: white;
     display: flex;
-    align-items: center;
     justify-content: center;
+    align-items: center;
     font-size: 20px;
     margin-bottom: 200px;
 `;
 
-const boxVariants = {
-    initial: (back: boolean) => {
-        return back ? { x: 500, opacity: 0 } : { x: -500, opacity: 0 };
-    },
+const Overlay = styled(motion.div)`
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
 
+const overlayVariants = {
+    initial: { opacity: 0 },
     visible: {
-        x: 0,
         opacity: 1,
-
-        // transition: {
-        //     duration: 1,
-        // },
+        transition: {
+            duration: 0.3,
+        },
     },
-    leaving: (back: boolean) => {
-        return back ? { x: -500, opacity: 0 } : { x: 500, opacity: 0 };
-    },
+    exit: { opacity: 0 },
 };
 
 function App() {
-    const [visible, setvisible] = useState(1);
-    const [back, setBack] = useState(false);
-    const nextIndex = () => {
-        setvisible((prev) => (prev === 10 ? 1 : prev + 1));
-        setBack(false);
-    };
-    const prevIndex = () => {
-        setvisible((prev) => (prev === 1 ? 10 : prev - 1));
-        setBack(true);
+    const [clicked, setClicked] = useState(false);
+    const [boxId, setBoxId] = useState('');
+    const toggleClicked = () => {
+        setClicked((prev) => !prev);
     };
 
     return (
-        <Wrapper>
-            <AnimatePresence custom={back}>
-                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
-                    i === visible ? (
+        <Wrapper onClick={toggleClicked}>
+            <Grid>
+                {['1', '2', '3', '4'].map((item) => (
+                    <Box
+                        onClick={() => setBoxId(item)}
+                        key={item}
+                        layoutId={item}
+                    >
+                        {item}
+                    </Box>
+                ))}
+            </Grid>
+            <AnimatePresence>
+                {clicked ? (
+                    <Overlay
+                        variants={overlayVariants}
+                        initial='initial'
+                        animate='visible'
+                        exit='exit'
+                        layoutId='overlay'
+                    >
                         <Box
-                            custom={back}
-                            variants={boxVariants}
-                            exit='leaving'
-                            initial='initial'
-                            animate='visible'
-                            key={i}
+                            layoutId={boxId}
+                            style={{ width: '400px', height: '200px' }}
                         >
-                            {i}
+                            {boxId}
                         </Box>
-                    ) : null
-                )}
+                    </Overlay>
+                ) : null}
             </AnimatePresence>
-            <button onClick={nextIndex}>Next</button>
-            <button onClick={prevIndex}>Prev</button>
         </Wrapper>
     );
 }
